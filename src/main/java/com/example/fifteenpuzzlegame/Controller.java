27678ee07c06003a7e1fa2925c03cd764facbf4c
lcj <lcj@kth.se>
2022-10-30 21:30:56 +0100

    Added scene fxml and allocated gridpane, a button for quitting and a game label

---
+++ src/main/java/com/example/fifteenpuzzlegame/Controller.java
@@ -0,0 +1,25 @@
+package com.example.fifteenpuzzlegame;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class Controller
+{
+    int numberOfMoves;
+    @FXML
+    private Label gameLabel;
+    @FXML
+    private GridPane puzzleGridPane;
+    @FXML
+    private Button quitButton;
+
+    public void startGame(){
+        gameLabel.setText("Number of moves: " + numberOfMoves);
+    }
+
+    public void quitGame(){
+        System.out.println("Game ended");
+    }
+}
